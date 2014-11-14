0fec47f31c9d9b77e4180fc1d85ed39a20f5e373
KC Theng <kai2.theng@gmail.com>
2014-11-14 17:16:21 +0800

    add gavick

---
+++ logs/com_virtuemart.log.php
@@ -0,0 +1,5 @@
+#
+#<?php die("Forbidden."); ?>
+
+2014-11-13 05:57:51 ERROR vmError: forSale Path/Url is not set correct :/home/project/public_html/templates/2013_11/administrator/components/com_virtuemart/vmfiles/
+2014-11-13 06:18:14 ERROR vmError: vmPdf: For the pdf, you must install the tcpdf library at /Applications/MAMP/htdocs/vichiSAMPLE/libraries/tcpdf
